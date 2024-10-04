072e2558254f4ce0ebb1201c697c32bcf03a4286
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2024-10-04 18:32:07 -0400

    Created project

---
+++ src/main/java/br/com/wandersontimoteo/api_rest_bancaria/ApiRestBancariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.wandersontimoteo.api_rest_bancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestBancariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wandersontimoteo/api_rest_bancaria/ApiRestBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wandersontimoteo.api_rest_bancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestBancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
